3134cca027996e0f363814c8f1fffc6f00f8c381
wdw_wei,8419 <1024475985@qq.com>
2019-08-28 09:09:19 +0800

    a

---
+++ User/main.c
@@ -0,0 +1,23 @@
+#include "stm32f0xx.h"
+#include "hal_GPIO.h"
+#include "hal_sys.h"
+#include "usart.h"
+#include "serial.h"
+
+
+int main(void)
+{
+	Delay_Init();
+	GPIO_CommonConfig();
+	USART1_Init(115200);
+	Serial_Rx_Init();
+	while(1)
+	{ 
+		delay_ms(500);
+		printf("nihao\r\n");
+		LED = 1;
+		delay_ms(500); 		
+		LED = 0;
+	}
+}
+
